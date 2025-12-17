7e2198f3a95a7ffa3b1ffbbff9549ce4c4d63e03
Faith Kemboi <kemboifaith941@gmail.com>
2025-12-17 18:09:40 +0300

    Create alx_book_store database using Python

---
+++ MySQLServer.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Create the alx_book_store database in MySQL server
+"""
+
+import mysql.connector
+from mysql.connector import Error
+
+
+def create_database():
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password=""
+        )
+
+        cursor = connection.cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+        print("Database 'alx_book_store' created successfully!")
+
+    except Error as e:
+        print(f"Error while connecting to MySQL: {e}")
+
+    finally:
+        if connection is not None and connection.is_connected():
+            cursor.close()
+            connection.close()
+
+
+if __name__ == "__main__":
+    create_database()
